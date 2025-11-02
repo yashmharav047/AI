96feabebe0bd1a9e59d5be8eccb02be1bf3fcab8
Madhav Pachakale <101622888+madhav0247@users.noreply.github.com>
2025-11-03 00:33:11 +0530

    added queris and results

--- family.pl
+++ family.pl
@@ -7,3 +7,18 @@ parent(mike, tom).
 % Rules
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+
+% Queries and Results
+
+% ?- sibling(mary, mike).
+% true.
+
+% ?- sibling(X, Y).
+% X = mary, Y = mike ;
+% X = mike, Y = mary.
+
+% ?- sibling(susan, tom).
+% false.
+
+% ?- sibling(mike, mary).
+% true.
